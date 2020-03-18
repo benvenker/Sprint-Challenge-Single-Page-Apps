473dbf21453a9a6e2f77b076c7debad9686df699
Ben Venker <bvemails@gmail.com>
2020-03-17 17:30:17 -0700

    good god, finally got individual character routing working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Character from "./components/Character.js";
 
 export default function App() {
   return (
-    <main data-testid='app'>
+    <main data-testid="app">
       <Header />
+      <Route exact path="/" render={props => <CharacterList {...props} />} />
+      <Route path="/character/:id" render={props => <Character {...props} />} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import "./CharacterList.css";
+import { BrowserRouter as Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [characters, setCharacters] = useState([]);
+
+  const urls2 = [];
+  const generateUrls = () => {
+    for (let i = 2; i <= 3; i++) {
+      urls2.push(
+        `https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/?page=${i}`
+      );
+    }
+  };
+  generateUrls();
+
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    // Practice getting all the data...
+    Promise.all(urls2.map(url => fetch(url)))
+      .then(responses => Promise.all(responses.map(res => res.json())))
+      .then(data =>
+        data.map(d => setCharacters(characters => [...characters, d.results]))
+      );
   }, []);
+
+  console.log("Character new array", characters);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Character List</h2>
+      <div className="characters">
+        {characters.map(characterArr =>
+          characterArr.map(character => {
+            // console.log("charactercard char", character);
+            return <div>{character.name}</div>;
+          })
+        )}
+      </div>
     </section>
   );
 }
